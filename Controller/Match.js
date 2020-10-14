20c2fd7efe8d2c1d05789e541667f426214e02d9
mads <mads.holmvang@hotmail.com>
2020-10-14 15:17:59 +0200

    .

--- Controller/Match.js
+++ Controller/Match.js
@@ -4,17 +4,17 @@ const match = require("../Models/Match");
 
 
 router.get("/", (req, res) =>{
-    res.json(match.myMatch);
+    res.json(match.myMatch); // Shows if there is a match
 })
 router.post("/",(req,res)=> {
-    res.json({"message" : "Match has been created"})
+    res.json({"message" : "Match has been created"}) // Shows that a match has been created
 });
 router.put("/", (req,res) => {
-    match.myMatch = match.newMatch
+    match.myMatch = match.newMatch //the match is updated to a the new match 
     res.json({"message": "Match updated"})
 });
 router.delete("/", (req,res) => {
-    match.myMatch = [];
+    match.myMatch = []; // The mathc is deleted.
     Users.newMatch = [];
     res.json({"message": "match deleted"})
 });
